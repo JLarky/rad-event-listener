804a9febb2f43fc93d3c53574a9f13110699b882
JLarky <jlarky@gmail.com>
2023-06-12 23:24:52 -0600

    add radEventListener test

--- main_test.ts
+++ main_test.ts
@@ -9,6 +9,32 @@ import {
 } from "https://deno.land/std@0.190.0/testing/mock.ts";
 import { radEventListener, rad } from "./mod.ts";
 
+Deno.test(function radEventListenerTest() {
+  const addEventListener = spy((type: "load", cb: () => void) => {});
+  const removeEventListener = spy((type: "load", cb: () => void) => {});
+  const onEvent = spy();
+  // setup
+  const cleanup = radEventListener(
+    { addEventListener, removeEventListener, onload: () => {} },
+    "load",
+    onEvent
+  );
+  assertSpyCalls(addEventListener, 1);
+  assertSpyCalls(removeEventListener, 0);
+  assertSpyCall(addEventListener, 0, {
+    args: ["load", onEvent],
+    returned: undefined,
+  });
+  // cleanup
+  assertEquals(cleanup(), undefined);
+  assertSpyCalls(addEventListener, 1);
+  assertSpyCalls(removeEventListener, 1);
+  assertSpyCall(removeEventListener, 0, {
+    args: ["load", onEvent],
+    returned: undefined,
+  });
+});
+
 Deno.test(function radTest() {
   const addEventListener = spy((type: "load", cb: () => void) => {});
   const removeEventListener = spy((type: "load", cb: () => void) => {});
