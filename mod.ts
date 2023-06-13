cef9577a9130a8681866289f1bae2a1f0b549ece
JLarky <jlarky@gmail.com>
2023-06-12 22:57:41 -0600

    fix types of radEventListener (solved by @ggrandi)
    - https://twitter.com/ggrandi04
    - https://github.com/ggrandi
    - https://tsplay.dev/N5xRMw
    - https://discord.com/channels/997886693233393714/1115858300043546734/1117154820319608943

--- mod.ts
+++ mod.ts
@@ -1,8 +1,24 @@
 export function radEventListener<
-  MyElement extends { addEventListener?: any; removeEventListener?: any }
+  MyElement extends { addEventListener: any; removeEventListener: any },
+  // get the possible events by using the `MyElement.on${someEvent}` properties
+  Event extends {
+    [K in keyof MyElement]-?: K extends `on${infer E}` ? E : never;
+  }[keyof MyElement]
 >(
   element: MyElement,
-  ...args: Parameters<MyElement["addEventListener"]>
+  // recreate the args for addEventListener
+  ...args: [
+    type: Event,
+    // grab the correct types off the function
+    listener: MyElement extends Record<
+      `on${Event}`,
+      null | ((...args: infer Args) => infer Return)
+    >
+      ? // overwrite the type of this to make sure that it is always `MyElement`
+        (this: MyElement, ...args: Args) => Return
+      : never,
+    options?: boolean | AddEventListenerOptions
+  ]
 ): () => void {
   element.addEventListener(...args);
   return () => {
